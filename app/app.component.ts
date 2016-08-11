6fa1670eb28a106e56a1b903015f1b4e8bf9a9a0
Jean Paul Attard <jeanp.attard@gmail.com>
2016-08-11 17:29:04 +0200

    Added iterative data, using *ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,6 +12,12 @@ export class Hero {
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+            <li *ngFor="let hero of heroes">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
         <h2>{{hero.name}} details!</h2>
         <div><label>id: </label>{{hero.id}}</div>
         <div>
@@ -27,4 +33,20 @@ export class AppComponent {
         id: 1,
         name: 'Windstorm'
     };
+
+    public heroes = HEROES;
 }
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Spider man' },
+    { id: 12, name: 'Bat Man' },
+    { id: 13, name: 'Super Man' },
+    { id: 14, name: 'Ent Man' },
+    { id: 15, name: 'Iron Man' },
+    { id: 16, name: 'Captain America' },
+    { id: 17, name: 'Black Widow' },
+    { id: 18, name: 'The Hulk' },
+    { id: 19, name: 'Hawk Eye' },
+    { id: 20, name: 'Green Lantern' }
+];
+
