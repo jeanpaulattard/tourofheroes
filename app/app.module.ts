6245520801b4a439b55549c916f854c9f7942de6
Jean Paul Attard <jeanp.attard@gmail.com>
2016-08-17 14:12:25 +0200

    Updated application with mock backend and db, as well updated services to start using HTTP requests.

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,20 +4,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule, XHRBackend } from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api/index';
 
 import { AppComponent } from './app.component';
 import { HeroDetailsComponent } from './herodetails/herodetail.component'
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './services/hero.service';
+import { InMemoryDataService } from './services/in-memory-data.service';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { routing } from './app.routing';
 
 @NgModule({
-    imports: [ BrowserModule, FormsModule, routing ],
+    imports: [ BrowserModule, FormsModule, routing, HttpModule ],
     declarations: [ AppComponent, HeroesComponent, HeroDetailsComponent, DashboardComponent ],
     bootstrap: [ AppComponent ],
-    providers: [ HeroService ]
+    providers: [
+        HeroService,
+        { provide: XHRBackend, useClass: InMemoryBackendService },
+        { provide: SEED_DATA, useClass: InMemoryDataService }
+    ]
 })
 
 export class AppModule {
--- app/services/hero.service.ts
+++ app/services/hero.service.ts
@@ -1,24 +1,81 @@
 /**
  * Created by jean-paul.attard on 12/08/2016.
  */
+import { Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 
-import { HEROES } from '../objects/mock-heroes';
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from '../objects/hero';
+import { Json } from '@angular/common/src/facade/lang';
 
 @Injectable()
 export class HeroService {
+    private heroesUrl: string = 'app/heroes';
+
+    constructor(private http: Http) {}
+
     getHeroes() {
-        return new Promise<Hero[]>(function (resolve) {
-            return setTimeout(function () {
-                return resolve(HEROES);
-            }, 200);
-        });
+        return this.http.get(this.heroesUrl)
+                   .toPromise()
+                   .then(response => response.json().data as Hero[])
+                   .catch(this.handleError);
+        // return new Promise<Hero[]>(function (resolve) {
+        //     return setTimeout(function () {
+        //         return resolve(HEROES);
+        //     }, 200);
+        // });
         // return new Promise<Hero[]>(resolve => setTimeout(() => resolve(HEROES), 200));
         // return Promise.resolve(HEROES);
     }
 
     getHero(id) {
         return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    // Save the hero
+    save(hero: Hero) {
+        if (hero.id) {
+            return this.put(hero);
+        }
+
+        return this.post(hero);
+    }
+
+    // Deletes an existing hero
+    delete(hero: Hero) {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+
+        let url = `${this.heroesUrl}/${hero.id}`;
+
+        return this.http.delete(url, { headers: headers }).toPromise()
+                   .catch(this.handleError);
+    }
+
+    // Add a new hero
+    private post(hero: Hero): Promise<Hero> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+
+        return this.http.post(this.heroesUrl, JSON.stringify(hero), { headers: headers })
+                   .toPromise().then(response => response.json().data)
+                   .catch(this.handleError);
+    }
+
+    // Update an existing hero
+    private put(hero: Hero) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        let url = `${this.heroesUrl}/${hero.id}`;
+
+        return this.http.put(url, JSON.stringify(hero), { headers: headers })
+                   .toPromise().then(response => response.json().data)
+                   .catch(this.handleError);
+    }
+
+
+    private handleError(error: any) {
+        console.error('An error occured:', error);
+        return Promise.reject(error.message || error);
     }
 }
