9d261a1076c256f4b5156438345e5696f6a08b4a
Jean Paul Attard <jeanp.attard@gmail.com>
2016-08-17 11:26:21 +0200

    Added routing from heroes component

--- app/heroes/heroes.component.ts
+++ app/heroes/heroes.component.ts
@@ -2,25 +2,31 @@
  * Created by jean-paul.attard on 12/08/2016.
  */
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from '../objects/hero';
-import { HeroDetailsComponent } from '../herodetails/herodetail.component';
 import { HeroService } from '../services/hero.service';
 
 @Component({
     selector: 'heroes',
     templateUrl: 'app/heroes/heroes.component.html',
-    styleUrls: ['app/heroes/heroes.component.css']
+    styleUrls: [ 'app/heroes/heroes.component.css' ]
 })
 
 export class HeroesComponent implements OnInit {
-    constructor(private heroService: HeroService) {}
+    constructor(private heroService: HeroService,
+                private router: Router) {}
 
     selectedHero: Hero;
     public heroes: Hero[];
 
     expandHero(hero: Hero) {
         this.selectedHero = hero;
+    }
+
+    gotoHero(hero: Hero) {
+        let link = [ '/details', hero.id ];
+        this.router.navigate(link);
     }
 
     getHeroes() {
