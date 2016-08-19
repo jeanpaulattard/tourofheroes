f0e7024fe1b2bb6279b44eddb7cd4a117207e192
Jean Paul Attard <jeanp.attard@gmail.com>
2016-08-19 10:57:02 +0200

    Updating routing to point to the correct registration component

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -7,7 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailsComponent } from './herodetails/herodetail.component';
-import { RegistrationFormComponent } from './registration/registration-form.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const appRoutes: Routes = [
     {
@@ -29,7 +29,7 @@ const appRoutes: Routes = [
     },
     {
         path: 'registration',
-        component: RegistrationFormComponent
+        component: RegistrationComponent
     }
 ];
 
