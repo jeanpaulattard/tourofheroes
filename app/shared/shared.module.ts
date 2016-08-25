38cb9fd6706dd4b36cd50d0953f277b5eee9b8cb
Jean Paul Attard <jeanp.attard@gmail.com>
2016-08-25 13:59:48 +0200

    Added some comments to describe why we are creating a forRoot() method.

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -15,8 +15,14 @@ import { LocalStorageService } from "./services/local-storage.service";
 export class SharedModule {
     /**
      * We want that providers are only included when imported by the RootModule. In the AppModule, we import the
-     * SharedModule with SharedModule.forRoot(). We shall not
-     * @returns {{ngModule: SharedModule, providers: LocalStorageService[]}}
+     * SharedModule with SharedModule.forRoot(). We shall not add Services to the providers section of the @NgModule
+     * decorator. By providing shared services to the root module, apart from being made available to the entire
+     * application, we are keeping the service as a singleton.
+     *
+     * If services are provided part of the decorator, a new instance is created whenever a module is lazy loaded. This
+     * is because the child injector of the lazy loaded module registers a new instance of the services.
+     *
+     * @returns {{ngModule: SharedModule, providers: (AuthenticationService|LocalStorageService)[]}}
      */
     static forRoot(): ModuleWithProviders {
         return {
