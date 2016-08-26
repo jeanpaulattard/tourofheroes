8ba2bdcbe8ff017034405037e2bf17fa6259231a
Jean Paul Attard <jeanp.attard@gmail.com>
2016-08-26 11:06:17 +0200

    Fixed sytle and template urls

--- app/+dashboard/hero-search/hero-search.component.ts
+++ app/+dashboard/hero-search/hero-search.component.ts
@@ -16,11 +16,11 @@ import 'rxjs/add/operator/catch';
 import { Hero } from '../../shared/hero';
 import { HeroSearchService } from './hero-search.service';
 
-
 @Component({
+    moduleId: module.id,
     selector: 'hero-search',
-    templateUrl: 'app/herosearch/hero-search.component.html',
-    styleUrls: [ 'app/herosearch/hero-search.component.css' ],
+    templateUrl: 'hero-search.component.html',
+    styleUrls: [ 'hero-search.component.css' ],
     providers: [ HeroSearchService ] // Specific to this component
 })
 
@@ -52,7 +52,8 @@ export class HeroSearchComponent implements OnInit {
         this.heroes = this.searchTerms
                           .debounceTime(300) // Wait for 300 ms
                           .distinctUntilChanged() // ignore if searchTerm is the same as previous one
-                          .switchMap(term => term ? this.heroSearchService.search(term) : Observable.of<Hero[]>([])) // Create a new observable each time
+                          // Create a new observable each time
+                          .switchMap(term => term ? this.heroSearchService.search(term) : Observable.of<Hero[]>([]))
                           .catch(error => {
                               console.log(error);
                               return Observable.of<Hero[]>([]);
