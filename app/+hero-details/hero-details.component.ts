070c29b6e9095da25a3a29736291ad7a21fb2874
Jean Paul Attard <jeanp.attard@gmail.com>
2016-08-30 11:48:56 +0200

    Updated forEach to a rxjs subscribtion

--- app/+hero-details/hero-details.component.ts
+++ app/+hero-details/hero-details.component.ts
@@ -1,19 +1,21 @@
 /**
  * Created by jean-paul.attard on 12/08/2016.
  */
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import { Hero } from '../shared/hero';
 import { HeroService } from '../core/services/hero.service';
 import { TitleWrapperService } from '../core/services/title-wrapper.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'hero-details.component.html',
     styleUrls: [ 'hero-details.component.css' ]
 })
-export class HeroDetailsComponent implements OnInit {
+export class HeroDetailsComponent implements OnInit, OnDestroy {
+
     constructor(private heroService: HeroService, private activeRoute: ActivatedRoute,
                 private titleWrapperService: TitleWrapperService) {
         this.titleWrapperService.setTitle('Heroe Details')
@@ -26,8 +28,17 @@ export class HeroDetailsComponent implements OnInit {
 
     id: number;
 
+    private subscription: Subscription;
+
     ngOnInit() {
-        this.activeRoute.params.forEach((params: Params) => {
+        /**
+         * Whenever a router navigates back and forth to the same component (example there's back and next button),
+         * it will reuse the instance of the component, and do not create one. Thus, since ngOnInit would only be
+         * called ones, subscribing to the params observable means that without re-createing the component, the we will
+         * always have the latest hero ID that is provided.
+         * @type {Subscription}
+         */
+        this.subscription = this.activeRoute.params.subscribe((params: Params) => {
             if (params[ 'id' ] !== undefined) {
                 let id = +params[ 'id' ];
                 this.heroService.getHero(id).then(hero => this.hero = hero);
@@ -37,6 +48,10 @@ export class HeroDetailsComponent implements OnInit {
                 this.hero = new Hero();
             }
         });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 
     save() {
