c76ba8559b9e9293f031e2cc78ae8debc543c084
JeanPaul Attard <jeanp.attard@gmail.com>
2016-09-06 11:57:45 +0200

    Updated application to RC6

--- app/+dashboard/hero-search/hero-search.service.ts
+++ app/+dashboard/hero-search/hero-search.service.ts
@@ -6,13 +6,16 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { Hero } from '../../shared/hero';
+import { Observable } from 'rxjs';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HeroSearchService {
 
     constructor(private http: Http) {}
 
-    search(term: string) {
+    search(term: string): Observable<Hero[]> {
         return this.http.get(`app/heroes/?name=${term}`).map((r: Response) => r.json().data as Hero[]);
     }
 }
