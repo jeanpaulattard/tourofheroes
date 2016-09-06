68ecf635065ff5927932d66aa73e1ce32e64bd9b
JeanPaul Attard <jeanp.attard@gmail.com>
2016-09-06 15:54:28 +0200

    Updated polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,6 +2,24 @@
  * Created by Jean-paul.attard on 06/09/2016.
  */
 
-import '../node_modules/core-js/client/shim.min';
-import '../node_modules/zone.js/dist/zone.js';
-import '../node_modules/reflect-metadata/Reflect';
+import 'core-js/es6/array';
+import 'core-js/es6/date';
+import 'core-js/es6/function';
+import 'core-js/es6/map';
+import 'core-js/es6/math';
+import 'core-js/es6/number';
+import 'core-js/es6/object';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/promise';
+import 'core-js/es6/reflect';
+import 'core-js/es6/regexp';
+import 'core-js/es6/set';
+import 'core-js/es6/string';
+import 'core-js/es6/symbol';
+import 'core-js/es6/weak-map'
+import 'core-js/es6/weak-set';
+
+import 'core-js/es7/reflect';
+
+import 'zone.js/dist/zone';
