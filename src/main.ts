71bf1bd2748b5141d772a5cecac8e69397e21088
JeanPaul Attard <jeanp.attard@gmail.com>
2016-09-06 13:32:48 +0200

    Step1. Moved the app folder, index.html, and css files under /src folder

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
