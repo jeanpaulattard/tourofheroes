650339615a2fc54099d2fface8f595da3a2b6aaa
JeanPaul Attard <jeanp.attard@gmail.com>
2016-09-06 13:46:45 +0200

    Added polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,7 @@
 /**
  * Created by Jean-paul.attard on 06/09/2016.
  */
+
+import '../node_modules/core-js/client/shim.min';
+import '../node_modules/zone.js/dist/zone.js';
+import '../node_modules/reflect-metadata/Reflect';
