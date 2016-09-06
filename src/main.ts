4a759c0c6c86b3c9d9fd73dcdb3aa105411527c6
JeanPaul Attard <jeanp.attard@gmail.com>
2016-09-06 15:55:17 +0200

    Added configuration for production

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 /**
  * Created by jean-paul.attard on 11/08/2016.
  */
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
+if (process.env.ENV === 'production') {
+    enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
