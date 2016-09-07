066a37c0b4e212c992e3b440c632d81cea4ea280
JeanPaul Attard <jeanp.attard@gmail.com>
2016-09-07 17:38:49 +0200

    Updated routing such that lazy loading is compatabile with webpack.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,10 @@
 /**
  * Created by jean-paul.attard on 12/08/2016.
  */
-
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BaseProtectedComponent } from './base-protected/base-protected.component';
-
 import { AuthGuard } from './core/services/route-guards/auth-guard.service';
 
 const appRoutes: Routes = [
@@ -16,13 +14,33 @@ const appRoutes: Routes = [
         component: BaseProtectedComponent,
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-            { path: 'heroes', loadChildren: 'app/+heroes/heroes.module' },
-            { path: 'dashboard', loadChildren: 'app/+dashboard/dashboard.module' }
+            {
+                path: 'heroes',
+                loadChildren: () => new Promise(resolve => (
+                    require as any).ensure([], () => resolve(require('./+heroes/heroes.module').default))
+                )
+            },
+            {
+                path: 'dashboard',
+                loadChildren: () => new Promise(resolve => (
+                    require as any).ensure([], () => resolve(require('./+dashboard/dashboard.module').default))
+                )
+            }
         ],
         canActivate: [ AuthGuard ]
     },
-    { path: 'registration', loadChildren: 'app/+registration/registration.module' },
-    { path: 'login', loadChildren: 'app/+login/login.module' }
+    {
+        path: 'registration',
+        loadChildren: () => new Promise(resolve => (
+            require as any).ensure([], () => resolve(require('./+registration/registration.module').default))
+        )
+    },
+    {
+        path: 'login',
+        loadChildren: () => new Promise(resolve =>
+            (require as any).ensure([], () => resolve(require('./+login/login.module').default))
+        )
+    }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
