e0bc80f8d4314d6793e1f1ad3957f2fdd69924a7
Jean Paul Attard <jeanp.attard@gmail.com>
2016-11-03 09:59:44 +0100

    Added my upper case to shared module; this fixed the compilation for ngc

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,17 @@
  */
 
 import { CommonModule } from '@angular/common';
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HighlightDirective } from './attribute-directives/highlight.directive';
 import { UnlessDirective } from './structural-directives/unless.directive';
+import { MyUppercasePipe } from './pipes/my-uppercase.pipe';
 
 @NgModule({
     imports: [],
-    declarations: [ HighlightDirective, UnlessDirective ],
-    exports: [ CommonModule, FormsModule, HighlightDirective, ReactiveFormsModule, UnlessDirective ]
+    declarations: [ HighlightDirective, UnlessDirective, MyUppercasePipe ],
+    exports: [ CommonModule, FormsModule, HighlightDirective, ReactiveFormsModule, UnlessDirective, MyUppercasePipe ]
 })
 export class SharedModule {
 }
