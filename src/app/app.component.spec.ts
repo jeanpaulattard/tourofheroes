45a69a3136783bd77835215f86f75b5450994348
Jean Paul Attard <jeanp.attard@gmail.com>
2016-11-07 12:36:28 +0100

    Fixed broken tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,13 @@
  * Created by Jean-paul.attard on 13/09/2016.
  */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
 import { configureTests } from './test.config';
+import { appRoutes } from './app.routing';
+import { SharedModule } from './shared/shared.module';
+import { BaseProtectedComponent } from './base-protected/base-protected.component';
 
 describe('App Component', () => {
     let fixture: ComponentFixture<AppComponent>;
@@ -12,7 +16,8 @@ describe('App Component', () => {
     beforeEach(done => {
         const configure = (testBed: TestBed) => {
             testBed.configureTestingModule({
-                imports: [ AppModule ]
+                imports: [ SharedModule, RouterTestingModule.withRoutes(appRoutes) ],
+                declarations: [ AppComponent, BaseProtectedComponent ]
             });
         };
 
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,7 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { BaseProtectedComponent } from './base-protected/base-protected.component';
 import { AuthGuard } from './core/services/route-guards/auth-guard.service';
 
-const appRoutes: Routes = [
+export const appRoutes: Routes = [
     { path: '', redirectTo: 'a', pathMatch: 'full' },
     {
         path: 'a',
